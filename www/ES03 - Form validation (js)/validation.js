3e86993c9d88c350bbe82988d5ea82595f8c8926
tommasosollo <tommaso.sollo2@gmail.com>
2024-10-23 18:45:01 +0000

    .

--- www/ES03 - Form validation (js)/validation.js
+++ www/ES03 - Form validation (js)/validation.js
@@ -98,10 +98,83 @@ function validateCF(elemento) {
 }
 
 function validateEmail(elemento) {
-    emailRegex = /^[A-Za-z.-_!+]+[@]{1}[A-Za-z]+[.]{1}[a-zA-Z]+/;
+    emailRegex = /[A-Za-z.-_!+]+[@]{1}[A-Za-z]+[.]{1}[a-zA-Z]+/;
     if (!emailRegex.test(elemento.value)) {
         elemento.style.border = '2px solid red';
     } else {
         elemento.style.border = '2px solid green';
     }
 }
+
+function validateTel(elemento) {
+    telRegex = /[0-9]{12}/;
+    if (!telRegex.test(elemento.value)) {
+        elemento.style.border = '2px solid red';
+    } else {
+        elemento.style.border = '2px solid green';
+    }
+}
+
+function validateVia(elemento) {
+    viaRegex = /^[a-zA-Z]+$/;
+    if (!viaRegex.test(elemento.value)) {
+        elemento.style.border = '2px solid red';
+    } else {
+        elemento.style.border = '2px solid green';
+    }
+}
+
+function validateNCivico(elemento) {
+    ncivicoRegex = /^[0-9]+$/;
+    if (!ncivicoRegex.test(elemento.value)) {
+        elemento.style.border = '2px solid red';
+    } else {
+        elemento.style.border = '2px solid green';
+    }
+}
+
+function validateCAP(elemento) {
+    capRegex = /^[0-9]{5}$/;
+    if (!capRegex.test(elemento.value)) {
+        elemento.style.border = '2px solid red';
+    } else {
+        elemento.style.border = '2px solid green';
+    }
+}
+
+function validateComune(elemento) {
+    comuneRegex = /^[a-zA-Z]+$/;
+    if (!comuneRegex.test(elemento.value)) {
+        elemento.style.border = '2px solid red';
+    } else {
+        elemento.style.border = '2px solid green';
+    }
+}
+
+function validateProvincia(elemento) {
+    provinciaRegex = /^[a-zA-Z]{2}$/;
+    if (!provinciaRegex.test(elemento.value)) {
+        elemento.style.border = '2px solid red';
+    } else {
+        elemento.style.border = '2px solid green';
+    }
+}
+
+function validateUsername(elemento) {
+    usernameRegex = /^[a-zA-Z0-9]+$/;
+    if (usernameRegex.test(elemento.value) && !(elemento.value.includes(inputs[0]) || elemento.value.includes(input[1]))) {
+        elemento.style.border = '2px solid green';
+    } else {
+        elemento.style.border = '2px solid red';
+    }
+}
+
+function validatePassword(elemento) {
+    passwordRegex = /{5}/;
+    if (!passwordRegex.test(elemento.value)) {
+        elemento.style.border = '2px solid red';
+    } else {
+        elemento.style.border = '2px solid green';
+    }
+}
+
