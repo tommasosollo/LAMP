b1a18f4afe43ec42a7b65fcfdfe48bd963885ce9
tommasosollo <tommaso.sollo2@gmail.com>
2024-12-11 12:54:45 +0000

    .

---
+++ www/JAVA/WebServer/TCPServer.java
@@ -0,0 +1,96 @@
+
+/**
+ * Implementazione di un server web utilizzando la comunicazione tramite socket.
+ * Lettura dati multi riga provenienti dal client
+ * 
+ * from folder network/..
+ * javac network/TcpServer.java; java network.TcpServer 
+ */
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPServer {
+	public static void main(String[] args) throws Exception {
+		
+		final int SERVER_PORT=8765;
+		String clientMsg = "";
+		
+		try {			 
+			// Creazione del socket sul server e ascolto sulla porta
+			ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+			System.out.println("Server: in ascolto sulla porta " + SERVER_PORT);
+
+			boolean endConn=false;
+			while(!endConn) {
+				// Attesa della connessione con il client
+				System.out.println("Attesa ricezione dati dal client ....................... \n");
+				Socket clientSocket = serverSocket.accept();
+				
+				// Create output stream to write data and input stream to read data from socket
+				DataOutputStream outStream = new DataOutputStream(clientSocket.getOutputStream());	
+				BufferedReader inStream = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+	
+				// ---------------------------------------------------------
+                //Lettura dati dal client un riga alla volta   
+                String checkLine = inStream.readLine();
+                System.out.println(checkLine);
+
+                while ((clientMsg = inStream.readLine()).length() != 0) {
+                    System.out.println(clientMsg);
+                }
+
+				// Elaborare qui i dati ricevuti dal client 
+                // ---------------------------------------------------------
+                
+                // Invio dei dati su stream di rete al client
+
+                
+                clientMsg = "HTTP/1.1 200 OK\r\n";
+                // clientMsg += "Connection: close\r\n";
+                // clientMsg += "Content-Type: text/plain\r\n";
+                clientMsg += "\r\n";
+                clientMsg += "Saluti dal web server Java";
+
+
+                
+                if (checkLine.contains("/ACCENDI")) {
+                    // Invio dei dati su stream di rete al client
+                    clientMsg = "HTTP/1.1 200 OK\r\n";
+                    // clientMsg += "Connection: close\r\n";
+                    // clientMsg += "Content-Type: text/plain\r\n";
+                    clientMsg += "\r\n";
+                    clientMsg += "Acceso";
+                }
+                else if (checkLine.contains("/SPEGNI")) {
+                    // Invio dei dati su stream di rete al client
+                    clientMsg = "HTTP/1.1 200 OK\r\n";
+                    // clientMsg += "Connection: close\r\n";
+                    // clientMsg += "Content-Type: text/plain\r\n";
+                    clientMsg += "\r\n";
+                    clientMsg += "Spento";
+                }
+                
+
+				
+                
+                outStream.write(clientMsg.getBytes());
+                
+				outStream.flush();
+
+				System.out.println("\n....................... Fine ricezione dati\n");
+				// Close resources
+				clientSocket.close();
+				inStream.close();
+				outStream.close();
+			}
+
+			// Close resources
+			serverSocket.close();
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
