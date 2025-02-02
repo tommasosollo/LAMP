21a3c425aaabbd8522c85e95c46d53528a049c47
tommasosollo <tommaso.sollo@erasmoinrete.it>
2025-02-02 14:25:41 +0100

    esercizio ES04 completato

--- www/ES04 - Gestione delle sessioni nelle applicazioni web/c/index.php
+++ www/ES04 - Gestione delle sessioni nelle applicazioni web/c/index.php
@@ -11,6 +11,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
     <title>Pagina di Benvenuto</title>
 </head>
 <body>
